9ce1be3bf92b10b49868f31dd4e298cce3b06702
hahamimidido <eason@tpe.hyweb.com.tw>
2020-03-14 16:30:57 +0800

    initial commit

---
+++ js/customize.js
@@ -0,0 +1,170 @@
+// 自行加入的JS請寫在這裡
+$(function() {
+    // lazyload  
+    //可以指定你想要的元素做lazyload
+    $("img").lazyload({ effect: "fadeIn" });
+    //燈箱slick+lightBox組合
+    $('.cp_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        focusOnSelect: true,
+        accessibility: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 545,
+            settings: {
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }]
+    });
+    $('.cp_slider').slickLightbox({
+        caption: 'caption',
+        useHistoryApi: 'true',
+        lazy: true
+    });
+    // cp_photo
+    $('.Slider-for').on('init reInit afterChange', function(event, slick, currentSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $('.controls').html(i + '/' + slick.slideCount);
+    });
+    $('.Slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.Slider-nav',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true
+    });
+    $('.Slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.Slider-for',
+        dots: true,
+        arrows: true,
+        focusOnSelect: true,
+        infinite: true,
+    });
+    // 
+    //請放置customize.js
+    $('.mpSlider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        fade: true,
+        cssEase: 'ease'
+    });
+    // 社會安全資訊網
+    $('.infoSlider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll:1,
+                    infinite: true,
+                   
+                }
+            }, {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }, {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+            , {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    // 統計資料
+    $('.statisticsSlider').slick({
+        dots: false,
+        infinite: false,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    
+                }
+            }, {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    //認識社會安全網限字數
+
+    var plan_len =200; // 超過200個字以"..."取代
+    $(".Planblock .plan p").each(function(i) {
+        if ($(this).text().length > plan_len) {
+            $(this).attr("title", $(this).text());
+            var text = $(this).text().substring(0, plan_len - 1) + "...";
+            $(this).text(text);
+        }
+    });
+
+
+});
