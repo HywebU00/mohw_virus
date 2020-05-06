5b14d6efbff6b35cb5faf9e82ecd328f93616127
hahamimidido <eason@tpe.hyweb.com.tw>
2020-05-06 17:06:57 +0800

    add lp timeline

--- js/hyui.js
+++ js/hyui.js
@@ -623,8 +623,7 @@ $(function() {
     /////////// category active  //////////
     /*-----------------------------------*/
     $('.category').find('a').off().click(function(event) {
-        $(this).parent('li').siblings().find('a').removeClass('active');
-        $(this).addClass('active');
+        $(this).toggleClass('active');
     });
     /*-----------------------------------*/
     /////////// 無障礙快捷鍵盤組合  //////////
